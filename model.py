698fd9717540f6756f73cd0c8809ab537d2e0bb7
taemin6697 <taemin6697@naver.com>
2023-05-10 20:57:33 +0900

    bert_custom_baseline

--- model.py
+++ model.py
@@ -10,12 +10,15 @@ import torch.nn.functional as F
 from torch.optim.lr_scheduler import StepLR
 from transformers import (AutoConfig, AutoModelForSequenceClassification,
                           AutoTokenizer)
+from adabelief_pytorch import AdaBelief
 
+from custom_model import CustomBertForSequenceClassification
 from utils import klue_re_micro_f1, lr_scheduler
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
 class ERNet(pl.LightningModule):
-    def __init__(self, config, wandb_config=None):
+    def __init__(self, config, wandb_config=None,state = None):
         super().__init__()
 
         if wandb_config == None:
@@ -27,7 +30,9 @@ class ERNet(pl.LightningModule):
 
         self.model_config = AutoConfig.from_pretrained(config["model"]["model_name"])
         self.model_config.num_labels = 30
-        self.model = AutoModelForSequenceClassification.from_pretrained(config["model"]["model_name"], config=self.model_config)
+        #self.model = AutoModelForSequenceClassification.from_pretrained(config["model"]["model_name"], config=self.model_config)
+        self.model = CustomBertForSequenceClassification.from_pretrained(config["model"]["model_name"], config=self.model_config,
+                                                                         state=state).to(device)
         self.lr_scheduler_type = config["train"]["lr_scheduler"]
 
         self.train_step = 0
@@ -42,7 +47,7 @@ class ERNet(pl.LightningModule):
         return x
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
+        optimizer = AdaBelief(self.parameters(), lr=self.learning_rate,weight_decouple=True,weight_decay=self.weight_decay)
         scheduler = lr_scheduler(lr_scheduler_type=self.lr_scheduler_type, optimizer=optimizer)
         return [optimizer], [scheduler]
 
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import pytorch_lightning as pl
 import pytz
 from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import CSVLogger, WandbLogger
 from transformers import AutoTokenizer
 
 from dataloader import ERDataModule
@@ -20,13 +21,20 @@ if __name__ == "__main__":
     
     MODEL_NAME = config["model"]["model_name"]
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    added_token_num = tokenizer.add_special_tokens({"additional_special_tokens": ["[ORG]", "[PER]", "[LOC]", "[POH]",
+                                                                                  "[DAT]", "[NOH]", "[/ORG]", "[/PER]",
+                                                                                  "[/LOC]", "[/POH]", "[/DAT]",
+                                                                                  "[/NOH]"]})
 
+    wandb_logger = WandbLogger(project='taemin-wandb-lightning', job_type='train')
     dataset_dir = os.path.join(prj_dir, os.pardir, "dataset", "train", "train.csv")
     dataloader = ERDataModule(config=config, tokenizer=tokenizer)
-    model = ERNet(config=config)
-
+    model = ERNet(config=config,state='train')
+    model.model.resize_token_embeddings(tokenizer.vocab_size + added_token_num)
+    print(model)
     now = datetime.now(pytz.timezone("Asia/Seoul"))
 
-    trainer = pl.Trainer(callbacks=ModelCheckpoint(dirpath=f"./checkpoint/{config['model']['model_name'].replace('/', '_')}/{now.strftime('%Y-%m-%d %H.%M.%S')}/", filename="{epoch}-{val_micro_f1:.2f}", monitor="val_micro_f1", mode="max"), max_epochs = config["train"]["num_train_epoch"])
+    trainer = pl.Trainer(callbacks=ModelCheckpoint(dirpath=f"./checkpoint/{config['model']['model_name'].replace('/', '_')}/{now.strftime('%Y-%m-%d_%H_%M_%S')}/", filename="{epoch}-{val_micro_f1=.2f}", monitor="val_micro_f1", mode="max"), max_epochs = config["train"]["num_train_epoch"],
+                         logger=wandb_logger)
     trainer.fit(model=model, train_dataloaders=dataloader)
 
