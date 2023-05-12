39af5cb9edc68cc8c978b7743c44df073fc97973
SangwonYoon <yoonsw0532@naver.com>
2023-05-12 07:09:15 +0000

    ainize tuning

--- train.py
+++ train.py
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     prj_dir = os.path.dirname(os.path.abspath(__file__))
     
     MODEL_NAME = config["model"]["model_name"]
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, additional_special_tokens = ["[SUB]", "[/SUB]", "[OBJ]", "[/OBJ]"])
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME,)
 
-    dataset_dir = os.path.join(prj_dir, os.pardir, "dataset", "train", "train.csv")
+    # dataset_dir = os.path.join(prj_dir, os.pardir, "dataset", "train", "train.csv")
     dataloader = ERDataModule(config=config, tokenizer=tokenizer)
     model = ERNet(config=config)
     model.model.resize_token_embeddings(len(tokenizer))
